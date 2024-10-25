c24751931389476e73d09aae392503b4f1fa4265
HAnnahc77 <HAnnahc77@nycstudents.net>
2024-10-25 13:56:42 -0400

    commit

--- stringsandbinary.js
+++ stringsandbinary.js
@@ -1,7 +1,24 @@
-function convertBinaryToDecimal(n) {
-  let binary = 01;
-  let convert = 2 ** n + 1;
-  for (let i = 0; i <= 255; i++) {
-    let decimal += binary * convert={;}
+function convertBinaryToDecimal(binary) {
+  let decimal = 0;
+  for (let i = 0; i < binary.length; i++) {
+    if (binary[binary.length - i - 1] === "1") {
+      decimal = decimal + Math.pow(2, i);
+    }
   }
+  return decimal;
 }
+
+function convertDecimalToBinary(decimal) {
+  let binary = "";
+  if (decimal === 0) {
+    return "0";
+  }
+  while (decimal > 0) {
+    let remainder = decimal % 2;
+    binary = remainder + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
+}
+
+console.log(convertDecimalToBinary("9"));
